b874f131326697b5e5715eff448b80ca8d6128fb
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2025-05-15 16:54:31 -0400

    about us 2

--- app/ui/aboutus/Aboutus.tsx
+++ app/ui/aboutus/Aboutus.tsx
@@ -9,13 +9,13 @@ const team = [
     name: "Jaime Guitron",
     role: "Co-Founder & Photographer",
     imageUrl:
-      "",
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
   {
     name: "Rizalyn Guitron",
     role: "Co-Founder & Photographer",
     imageUrl:
-      "",
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
 ];
 
