fe061bd9f65ef2611bf50dee8e4942426c50369c
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2025-05-15 11:37:54 -0400

    fix img

--- app/ui/components/content/ImageList/ImageList.tsx
+++ app/ui/components/content/ImageList/ImageList.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 export interface CollectionProps {
   sets?: {
@@ -30,14 +31,15 @@ export default function ImageList({
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
           {sets.map((set) => (
-            <a key={set.slug} href={"product.href"} className="group">
-              <img
-                alt={set?.asset?.alt}
-                src={set?.asset?.url}
-                className="aspect-square w-full overflow-hidden rounded-lg object-cover group-hover:opacity-75 sm:aspect-2/3"
-              />
-              {/* <p>test</p> */}
-            </a>
+            <Image
+              alt={set?.asset?.alt || ""}
+              src={set?.asset?.url || ""}
+              width={550}
+              height={400}
+              loading="lazy"
+              className="rounded-lg"
+              key={set.slug || ""}
+            />
           ))}
         </div>
       </div>
