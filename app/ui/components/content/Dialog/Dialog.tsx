8d068e63615b6cdc639fb267c19a4371fd064c8a
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2025-06-13 16:59:16 -0400

    dialog image load fix

--- app/ui/components/content/Dialog/Dialog.tsx
+++ app/ui/components/content/Dialog/Dialog.tsx
@@ -25,15 +25,15 @@ export default function ImageDialog({
     >
       <DialogBackdrop
         transition
-        className="fixed inset-0 hidden bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in md:block"
+        className="fixed inset-0 hidden bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-200 data-leave:ease-in md:block"
       />
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+        <div className="flex min-h-full items-stretch justify-center text-center md:items-center  lg:px-4">
           <DialogPanel
             transition
             className="flex w-full transform text-left text-base transition data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in md:my-8 md:max-w-2xl md:px-4 data-closed:md:translate-y-0 data-closed:md:scale-95 lg:max-w-4xl"
           >
-            <div className="relative flex  items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+            <div className="relative flex m-0 md:m-12 items-center overflow-hidden bg-white pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
               <button
                 type="button"
                 onClick={() => onClose?.(false)}
@@ -42,14 +42,14 @@ export default function ImageDialog({
                 <span className="sr-only">Close</span>
                 <XMarkIcon aria-hidden="true" className="size-6" />
               </button>
-
-              <div className="grid grid-cols-1 items-start lg:items-center">
+              <div className="grid grid-cols-1 items-start lg:items-center height-full">
                 <Image
                   alt={alt || "Image"}
                   src={url || ""}
-                  className="max-w-full max-h-screen w-auto h-auto rounded-lg bg-gray-100 object-contain"
+                  className="max-w-full max-h-lvh w-auto h-lvh rounded-lg bg-gray-100 object-cover"
                   height={800}
                   width={800}
+                  loading="lazy"
                 />
               </div>
             </div>
